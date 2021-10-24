db615dcf9513385df40ebd30369a185b005c54e1
YashShukla025 <yashshukla048@gmail.com>
2021-10-24 11:02:52 +0100

    Second Commit with updated code

--- src/main/java/ie/tcd/yshukla/Indexing.java
+++ src/main/java/ie/tcd/yshukla/Indexing.java
@@ -1,115 +1,164 @@
 package ie.tcd.yshukla;
-import java.io.IOException;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 import java.io.BufferedReader;
-import java.io.FileReader;
-import java.nio.file.Paths;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Date;
 
 import org.apache.lucene.analysis.Analyzer;
+import org.apache.lucene.analysis.core.SimpleAnalyzer;
+import org.apache.lucene.analysis.core.WhitespaceAnalyzer;
 import org.apache.lucene.analysis.standard.StandardAnalyzer;
 import org.apache.lucene.analysis.en.EnglishAnalyzer;
-import org.apache.lucene.document.Document;
-import org.apache.lucene.document.Field;
-import org.apache.lucene.document.TextField;
-import org.apache.lucene.document.StringField;
+import org.apache.lucene.document.*;
 import org.apache.lucene.index.IndexWriter;
+import org.apache.lucene.index.IndexWriterConfig.OpenMode;
 import org.apache.lucene.index.IndexWriterConfig;
+import org.apache.lucene.search.similarities.*;
 import org.apache.lucene.store.Directory;
 import org.apache.lucene.store.FSDirectory;
-import org.apache.lucene.search.similarities.*;
-import org.apache.lucene.search.similarities.TFIDFSimilarity;
 
-import org.apache.commons.lang3.StringUtils;
+public class Indexing {
 
- 
-public class Indexing
-{
-	
-	// Directory where the search index will be saved
-	private static String INDEX_DIRECTORY = "index";
-	
+    /** Index all text files under a directory. */
+    public void index() {
+        String indexEnglish = "Index/English_analyzer";
+        String indexStandard = "index/standard_analyzer";
+        String cran_path = "cran/cran.all.1400";
 
-	public static void main(String[] args) throws IOException
-	{
-		// Make sure we were given something to index
-		if (args.length <= 0)
-		{
-            System.out.println("Expected crandata as input");
-            System.exit(1);            
+        final Path cranPath = Paths.get(cran_path);
+
+        if (!Files.isReadable(cranPath)) {
+            System.out.println("Document directory '" + cranPath.toAbsolutePath() + "' does not exist or is not readable, please check the path");
+            System.exit(1);
         }
 
-		// Analyzer that is used to process TextField
-		//Analyzer analyzer = new StandardAnalyzer();
-		Analyzer englishAnalyzer = new EnglishAnalyzer();
-		
-		
-		// ArrayList of documents in the corpus
-		ArrayList<Document> documents = new ArrayList<Document>();
+        Date start = new Date();
+        try {
 
-		// Open the directory that contains the search index
-		Directory directory = FSDirectory.open(Paths.get(INDEX_DIRECTORY));
+            Directory indexEnglishDirectory = FSDirectory.open(Paths.get(indexEnglish));
+            Directory indexStandardDirectory = FSDirectory.open(Paths.get(indexStandard));
 
-		// Set up an index writer to add process and save documents to the index
-		IndexWriterConfig config = new IndexWriterConfig(englishAnalyzer);
-		config.setOpenMode(IndexWriterConfig.OpenMode.CREATE);
-		
-		config.setSimilarity(new MultiSimilarity(new Similarity[]{new ClassicSimilarity(),new LMDirichletSimilarity(),new BM25Similarity()}));
+           
+            Analyzer sAnalyzer = new StandardAnalyzer();
+            Analyzer eAnalyser = new EnglishAnalyzer();
+            //Analyzer analyzer = new SimpleAnalyzer();
+            //Analyzer analyzer = new WhitespaceAnalyzer();
 
-		IndexWriter iwriter = new IndexWriter(directory, config);
-		
-		String queriesPath = "cran/cran.";
-		BufferedReader reader = new BufferedReader(new FileReader(args[0]));
-		String contentLine = reader.readLine();
-		
-		StringBuffer dataset = new StringBuffer();
-		
-		while (contentLine != null) {
-			dataset.append(contentLine+"\n");
-		    contentLine = reader.readLine();
-		}
-	    reader.close();
-	    
-	    HashMap<String,String> map = new HashMap<String,String>();
-	    for(int i=1;i<1401;i++) {
-	    	System.out.println(String.format("Indexing the %d document", i));
-	    	String result = StringUtils.substringBetween(dataset.toString(), ".I "+String.valueOf(i), ".I "+String.valueOf(i+1));
-		    String title = StringUtils.substringBetween(result, ".T", ".A");
-		    String authour = StringUtils.substringBetween(result, ".A", ".B");
-		    String biblo = StringUtils.substringBetween(result, ".B", ".W");
-		    String content = result.substring(result.indexOf(".W\n")+3);
-		    
-		    //Create a new document
-		    Document doc = new Document();
-		    doc.add(new StringField("Document_ID",String.valueOf(i),Field.Store.YES));
-			doc.add(new TextField("Document_Title", title, Field.Store.YES));
-			doc.add(new TextField("Document_Authour", authour, Field.Store.YES));
-			doc.add(new TextField("Document_Biblo", biblo, Field.Store.YES));
-			doc.add(new TextField("Document_Content", content, Field.Store.YES));
-			
-			documents.add(doc);
-	    }
-	   
-		// Write all the documents in the linked list to the search index
-		iwriter.addDocuments(documents);
-		
-		//Using writer.forceMerge to maximise search performance.
-		iwriter.forceMerge(1);
+			IndexWriterConfig iWCEnglish = new IndexWriterConfig(eAnalyser);
+            IndexWriterConfig iWCStandard = new IndexWriterConfig(sAnalyzer);
 
-		// Commit everything and close
-		iwriter.close();
-		directory.close();
-		
-//		try{
-//			Querysearcher querySearcher = new Querysearcher();
-//			//querySearcher.searchQuery();
-//			querySearcher.searchQueryFromFile();
-//		}catch(Exception e) {
-//			e.printStackTrace();
-//		}
-	}
+            //BM25 Similarity
+            //iwc.setSimilarity(new BM25Similarity());
+
+            //Classic Similarity
+            //iwc.setSimilarity(new ClassicSimilarity());
+
+            //LMDirichletSimilarity
+            //iwc.setSimilarity(new LMDirichletSimilarity());
+
+            //Trying a multi similarity model
+            iWCEnglish.setSimilarity(new MultiSimilarity(new Similarity[]{new BM25Similarity(),new ClassicSimilarity()}));
+            iWCStandard.setSimilarity(new MultiSimilarity(new Similarity[]{new BM25Similarity(),new ClassicSimilarity()}));
+
+            //Trying another multi similarity model
+            //iwc.setSimilarity(new MultiSimilarity(new Similarity[]{new BM25Similarity(),new LMDirichletSimilarity()}));
+
+            //Trying another multi similarity model
+            //iwc.setSimilarity(new MultiSimilarity(new Similarity[]{new ClassicSimilarity(),new LMDirichletSimilarity()}));
+
+            iWCEnglish.setOpenMode(OpenMode.CREATE);
+            iWCStandard.setOpenMode(OpenMode.CREATE);
+
+            IndexWriter indexWriter1 = new IndexWriter(indexEnglishDirectory, iWCEnglish);
+            IndexWriter indexWriter2 = new IndexWriter(indexStandardDirectory, iWCStandard);
+            
+            indexDoc(indexWriter1, cranPath);
+            indexDoc(indexWriter2, cranPath);
+
+            //Using writer.forceMerge to maximise search performance.
+            indexWriter1.forceMerge(1);
+            indexWriter2.forceMerge(1);
+
+            indexWriter1.close();
+            indexWriter2.close();
+
+            Date end = new Date();
+            System.out.println(end.getTime() - start.getTime() + " total milliseconds");
+
+        } catch (IOException e) {
+            System.out.println(" Exception: " + e.getClass() + "\n with message: " + e.getMessage());
+        }
+    }
+
+    /** Indexes the 'cran.all.1400' file */
+    static void indexDoc(IndexWriter writer, Path file) throws IOException {
+        try (InputStream is = Files.newInputStream(file)) {
+
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(is, StandardCharsets.UTF_8));
+            Boolean first = true;
+            System.out.println("Indexing documents....");
+
+            String line = bufferedReader.readLine();
+            String contnt = "";
+            while(line != null){
+                Document doc = new Document();
+                if(line.startsWith(".I")){
+                    /*
+                     * I think the ID of the document does not make sense to be analysed,
+                     * hence it is just directly stored without any analysis.
+                     */
+                    doc.add(new StringField("ID", line.substring(3), Field.Store.YES));
+                    line = bufferedReader.readLine();
+                }
+                if (line.startsWith(".T")){
+                	line = bufferedReader.readLine();
+                    while(!line.startsWith(".A")){
+                        contnt += line + " ";
+                        line = bufferedReader.readLine();
+                    }
+                    doc.add(new TextField("Title", contnt, Field.Store.YES));
+                    contnt = "";
+                }
+                if (line.startsWith(".A")){
+                	line = bufferedReader.readLine();
+                    while(!line.startsWith(".B")){
+                        contnt += line + " ";
+                        line = bufferedReader.readLine();
+                    }
+                    doc.add(new TextField("Author", contnt, Field.Store.YES));
+                    contnt = "";
+                }
+                if (line.startsWith(".B")){
+                	line = bufferedReader.readLine();
+                    while(!line.startsWith(".W")){
+                        contnt += line + " ";
+                        line = bufferedReader.readLine();
+                    }
+                    /*
+                     * After a bit of analysis, I found that for this dataset, analysing
+                     * and storing bibliography details proved to be slightly inefficient.
+                     */
+                    doc.add(new StringField("Biblography", contnt, Field.Store.YES));
+                    contnt = "";
+                }
+                if (line.startsWith(".W")){
+                	line = bufferedReader.readLine();
+                    while(line != null && !line.startsWith(".I")){
+                        contnt += line + " ";
+                        line = bufferedReader.readLine();
+                    }
+                    //Not storing the words in an attempt to save storage space.
+                    doc.add(new TextField("words", contnt, Field.Store.NO));
+                    contnt = "";
+                }
+                writer.addDocument(doc);
+            }
+        }
+    }
 }
--- src/main/java/ie/tcd/yshukla/Querysearcher.java
+++ src/main/java/ie/tcd/yshukla/Querysearcher.java
@@ -1,105 +1,78 @@
 package ie.tcd.yshukla;
-
 import java.io.BufferedReader;
-import java.io.FileReader;
 import java.io.IOException;
-import java.io.PrintWriter;
 import java.nio.charset.StandardCharsets;
-
-import java.nio.file.Paths;
-import java.util.HashMap;
 import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.io.PrintWriter;
 
-import org.apache.commons.lang3.StringUtils;
 import org.apache.lucene.analysis.Analyzer;
-import org.apache.lucene.analysis.standard.StandardAnalyzer;
-import org.apache.lucene.analysis.en.EnglishAnalyzer;
-
-import org.apache.lucene.document.Document;
-
-import org.apache.lucene.store.Directory;
-import org.apache.lucene.store.FSDirectory;
-
-import org.apache.lucene.index.Term;
-import org.apache.lucene.queryparser.classic.MultiFieldQueryParser;
-import org.apache.lucene.queryparser.classic.ParseException;
-import org.apache.lucene.index.DirectoryReader;
-
-import org.apache.lucene.search.Query;
-import org.apache.lucene.search.ScoreDoc;
-import org.apache.lucene.search.TermQuery;
-import org.apache.lucene.search.IndexSearcher;
-import org.apache.lucene.search.BooleanQuery;
-import org.apache.lucene.search.BooleanClause;
-
 import org.apache.lucene.analysis.core.SimpleAnalyzer;
 import org.apache.lucene.analysis.core.WhitespaceAnalyzer;
+import org.apache.lucene.analysis.standard.StandardAnalyzer;
+import org.apache.lucene.analysis.en.EnglishAnalyzer;
+import org.apache.lucene.document.Document;
+import org.apache.lucene.index.DirectoryReader;
 import org.apache.lucene.index.IndexReader;
 import org.apache.lucene.queryparser.classic.QueryParser;
+import org.apache.lucene.queryparser.classic.MultiFieldQueryParser;
+import org.apache.lucene.search.IndexSearcher;
+import org.apache.lucene.search.Query;
+import org.apache.lucene.search.ScoreDoc;
 import org.apache.lucene.search.TopDocs;
 import org.apache.lucene.search.similarities.*;
+import org.apache.lucene.store.FSDirectory;
 
+/** Simple command-line based search demo. */
 public class Querysearcher {
-	// the location of the search index
-	private static String INDEX_DIRECTORY = "index";
-	
-	// Limit the number of search results we get
-	private static int MAX_RESULTS = 100;
 
-	public void searchQuery() throws IOException
-	{
-		// Open the folder that contains our search index
-		Directory directory = FSDirectory.open(Paths.get(INDEX_DIRECTORY));
-		
-		// create objects to read and search across the index
-		DirectoryReader ireader = DirectoryReader.open(directory);
-		IndexSearcher isearcher = new IndexSearcher(ireader);
+    public void srch() throws Exception {
 
-		// builder class for creating our query
-		BooleanQuery.Builder query = new BooleanQuery.Builder();
-
-		// Some words that we want to find and the field in which we expect
-		// to find them
-		Query term1 = new TermQuery(new Term("Document_Content", "transition"));
-
-//		// construct our query using basic boolean operations.
-		query.add(new BooleanClause(term1, BooleanClause.Occur.SHOULD));   // AND
-//		query.add(new BooleanClause(term2, BooleanClause.Occur.MUST));     // OR
-//		query.add(new BooleanClause(term3, BooleanClause.Occur.MUST_NOT)); // NOT
-
-		// Get the set of results from the searcher
-		ScoreDoc[] hits = isearcher.search(query.build(), MAX_RESULTS).scoreDocs;
-		
-		// Print the results
-		System.out.println("Documents: " + hits.length);
-		for (int i = 0; i < hits.length; i++)
-		{
-			Document hitDoc = isearcher.doc(hits[i].doc);
-			System.out.println(i + ") " + hitDoc.get("Document_Title") + " " + hits[i].score);
-		}
-
-		// close everything we used
-		ireader.close();
-		directory.close();
-	}
-	
-	public void searchQueryFromFile() throws Exception
-	{
-		String index = "index";
+    	String indexEnglish = "Index/English_analyzer";
+        String indexStandard = "index/standard_analyzer";
         String queryString = "";
+
+        IndexReader reader1 = DirectoryReader.open(FSDirectory.open(Paths.get(indexEnglish)));
+        IndexReader reader2 = DirectoryReader.open(FSDirectory.open(Paths.get(indexStandard)));
         
-        Analyzer analyzer = new EnglishAnalyzer();
+        IndexSearcher search_english = new IndexSearcher(reader1);
+        IndexSearcher search_stndrd = new IndexSearcher(reader2);
+
+        //Analyzer analyzer = new SimpleAnalyzer();
+        //Analyzer analyzer = new WhitespaceAnalyzer();
+        Analyzer sAnalyzer = new StandardAnalyzer();
+        Analyzer eAnalyser = new EnglishAnalyzer();
+
+        String path_english = "results_english.txt";
+        String path_stndrd = "results_standard.txt";
         
-        IndexReader reader = DirectoryReader.open(FSDirectory.open(Paths.get(index)));
-        IndexSearcher searcher = new IndexSearcher(reader);
-        searcher.setSimilarity(new MultiSimilarity(new Similarity[]{new ClassicSimilarity(),new LMDirichletSimilarity(),new BM25Similarity()}));
-		
-        String results_path = "results.txt";
-        PrintWriter writer = new PrintWriter(results_path, "UTF-8");
-        
-		String queriesPath = "cran/cran.qry";
+        PrintWriter pwenglish = new PrintWriter(path_english, "UTF-8");
+        PrintWriter pwstndrd = new PrintWriter(path_stndrd, "UTF-8");
+
+        //BM25 Similarity
+        //searcher.setSimilarity(new BM25Similarity());
+
+        //Classic Similarity
+        //searcher.setSimilarity(new ClassicSimilarity());
+
+        //LMDirichletSimilarity
+        //searcher.setSimilarity(new LMDirichletSimilarity());
+
+        //Trying a multi similarity model
+        search_english.setSimilarity(new MultiSimilarity(new Similarity[]{new BM25Similarity(),new ClassicSimilarity()}));
+        search_stndrd.setSimilarity(new MultiSimilarity(new Similarity[]{new BM25Similarity(),new ClassicSimilarity()}));
+
+        //Trying another multi similarity model
+        //searcher.setSimilarity(new MultiSimilarity(new Similarity[]{new BM25Similarity(),new LMDirichletSimilarity()}));
+
+        //Trying another multi similarity model
+        //searcher.setSimilarity(new MultiSimilarity(new Similarity[]{new ClassicSimilarity(),new LMDirichletSimilarity()}));
+
+        String queriesPath = "cran/cran.qry";
         BufferedReader bufferedReader = Files.newBufferedReader(Paths.get(queriesPath), StandardCharsets.UTF_8);
-        MultiFieldQueryParser parser = new MultiFieldQueryParser(new String[]{"Document_ID", "Document_Title", "Document_Authour", "Document_Biblo","Document_Content"}, analyzer);
+        MultiFieldQueryParser englishParser = new MultiFieldQueryParser(new String[]{"title", "words"}, eAnalyser);
+        MultiFieldQueryParser standardParser = new MultiFieldQueryParser(new String[]{"title", "words"}, sAnalyzer);
+
         String currentLine = bufferedReader.readLine();
 
         System.out.println("Reading in queries and creating search results.");
@@ -121,29 +94,30 @@ public class Querysearcher {
                 }
             }
             queryString = queryString.trim();
-            Query query = parser.parse(QueryParser.escape(queryString));
+            Query query1 = englishParser.parse(QueryParser.escape(queryString));
+            Query query2 = standardParser.parse(QueryParser.escape(queryString));
             queryString = "";
-            StringBuffer results = searchIndex(searcher, Integer.parseInt(id), query);
-            writer.print(results.toString());
+            performSearch(search_english, pwenglish, Integer.parseInt(id), query1);
+            performSearch(search_stndrd, pwstndrd, Integer.parseInt(id), query2);
         }
 
-        System.out.println("Results have been written to the 'results.txt' file.");
-        writer.close();
-        reader.close();
-	}
-	
-	// Performs search and writes results to the writer
-    public static StringBuffer searchIndex(IndexSearcher searcher, Integer queryNumber, Query query) throws IOException {
-    	StringBuffer result = new StringBuffer();
-        TopDocs results = searcher.search(query, 1400);
+        System.out.println("Results have been written to the 'results_english.txt' and 'results_standard.txt' file.");
+        pwenglish.close();
+        pwstndrd.close();
+        reader1.close();
+        reader2.close();
+    }
+
+
+    // Performs search and writes results to the writer
+    public static void performSearch(IndexSearcher searcher, PrintWriter writer, Integer queryNumber, Query query) throws IOException {
+        TopDocs results = searcher.search(query, 999);
         ScoreDoc[] hits = results.scoreDocs;
 
         // To write the results for each hit in the format expected by the trec_eval tool.
         for (int i = 0; i < hits.length; i++) {
             Document doc = searcher.doc(hits[i].doc);
-            result.append(queryNumber+" Q0 "+ doc.get("Document_ID") + " " + i + " " + hits[i].score + " STANDARD\n");
+            writer.println(queryNumber + " 0 " + doc.get("id") + " " + i + " " + hits[i].score + " ANALYSIS");
         }
-        
-        return result;
     }
 }
