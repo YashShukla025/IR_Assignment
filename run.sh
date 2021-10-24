1f3367e9c871d03b259ff6e6dd13e1ec4b1786fd
YashShukla025 <yashshukla048@gmail.com>
2021-10-24 20:55:25 +0100

    update run.sh

--- run.sh
+++ run.sh
@@ -5,3 +5,7 @@ java -jar target/Lucene-Example-0.0.1-SNAPSHOT.jar
 ./trec_eval/trec_eval -m runid -m map -m P.5 -m gm_map cran/cranqrell results_standard.txt
 ./trec_eval/trec_eval -m runid -m map -m P.5 -m gm_map cran/cranqrell results_english.txt
 ./trec_eval/trec_eval -m runid -m map -m P.5 -m gm_map cran/cranqrell results_simple.txt
+./trec_eval/trec_eval -m runid -m map -m P.5 -m gm_map cran/cranqrell results_standard_BM25.txt
+./trec_eval/trec_eval -m runid -m map -m P.5 -m gm_map cran/cranqrell results_english_BM25.txt
+./trec_eval/trec_eval -m runid -m map -m P.5 -m gm_map cran/cranqrell results_simple_BM25.txt
+
